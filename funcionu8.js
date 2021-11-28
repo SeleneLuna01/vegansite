a7c2a15a6f6645dd75b03e7f3b1f530cef890999
SeleneLuna01 <lunaselene454@gmail.com>
2021-11-28 17:24:19 -0300

    my first website

---
+++ funcionu8.js
@@ -0,0 +1,9 @@
+function mostrar(bloque) {
+    var caja_mostrar = document.getElementById(bloque)
+    caja_mostrar.style.display = 'block'
+}
+
+function ocultar(bloque){
+    var caja_ocultar = document.getElementById(bloque)
+    caja_ocultar.style.display ='none'
+}
